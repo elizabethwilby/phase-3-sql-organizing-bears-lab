cbab832852a45a2a516f95ce59e58f15598cdb7d
elizabethwilby <elizabethwilby@Elizabeths-MacBook-Pro.local>
2026-07-15 10:38:30 -0400

    organized bears

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 5, 'M', 'brown', 'friendly', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 3, 'M', 'black', 'energetic', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 7, 'F', 'brown', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 10, 'M', 'brown', 'grumpy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 4, 'F', 'blonde', 'playful', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 6, 'M', 'green', 'mean', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 2, 'F', 'white', 'curious', 1);
+
+INSERT INTO bears (age, sex, color, temperament, alive)
+VALUES (1, 'F', 'black', 'shy', 1);
